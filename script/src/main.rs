b5befc1295947affabd8772fafc273942fcd67fe
Drew Tada <drew.tada@gmail.com>
2024-02-26 17:25:03 -0500

    two moves working

--- script/src/main.rs
+++ script/src/main.rs
@@ -11,17 +11,22 @@ struct Move {
 
 fn main() {
     let white_pub = "562099bd28942798feff232b5601f2de46ab234ed121d6c9edd11375dd1c36a0";
-    let black_pub = "2bc031cac37ac417b5ba436f8beb89199c52b7996ad855691abe7f6c1248f6d3";
+    let black_pub = "9446cce349ab15d3740bfd4e10c6702d1bacf602149a730e6c8c10228ae683df";
 
     let white_move_1 = Move {
         san: "6534".to_string(), // e4 in hex
         sig: "db0509435b2645ef7f5526974ba32fb1c8f2a5704ae900128c3a2013a6f98fef98b3f8b8b16d7a35e143f886c85e1b1c37b05673a253cd46d696f9dff91fc702".to_string(),
     };
 
+    let black_move_1 = Move {
+        san: "6335".to_string(), // c5 in hex
+        sig: "801341a63d1a1c42893a7b90286632b6a826ec967241d4bfb2b3fcd86728dc7bd3272599ccfd496c872cf704215b01c72a0398cb2a21c1596beb0f05c13f640e".to_string(),
+    };
+
     let mut stdin = SP1Stdin::new();
     stdin.write(&white_pub);
     stdin.write(&black_pub);
-    stdin.write(&vec![white_move_1]);
+    stdin.write(&vec![white_move_1, black_move_1]);
 
     let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
 
